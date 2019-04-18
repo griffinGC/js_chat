41b2fa8d80d17887e2eb11456e04f8cc3cc15c36
griffinGC <griffinDouble@gmail.com>
2019-04-18 13:41:11 +0900

    dev_init

--- schemas/chat.js
+++ schemas/chat.js
@@ -4,6 +4,7 @@ const {Shema} = mongoose;
 const{Types : {ObjectId}} = Schema;
 const chatSchema = new Schema({
     room:{
+        //이걸 위해서 Types :{ObjectId} 이걸로 불러옴
         type : ObjectId,
         required :true,
         ref : 'Room'
