60669f3faf8319b45a6a3f3c4a528dd4a38ea6a0
griffinGC <griffinDouble@gmail.com>
2019-04-18 12:54:56 +0900

    init

---
+++ schemas/room.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
+
+const roomSchema = new Schema({
+    title :{
+        type :String,
+        required : true
+    },
+    max :{
+        type : Number,
+        required : true,
+    },
+    owner :{
+        type : String,
+        required : true
+    },
+    password : String,
+    createdAt :{
+        type : Date,
+        default : Date.now
+    },
+});
+
+module.exports = mongoose.model('Room',roomSchema);
